0d1a63a5f525266a938dc92f337e86695777f892
Nicholas George <nicholasgeorge@Imperators-iMac-2.local>
2023-10-05 22:16:15 -0700

    This commit includes initial files and web scraping capability.

---
+++ PyAviationAccidentsSetupConstants.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#*******************************************************************************************
+ #
+ #  File Name:  PyAviationAccidentsSetupConstants.py
+ #
+ #  File Description:
+ #      This Python script, PyAviationAccidentsSetupConstants.py, contains generic 
+ #      Python constants for completing common tasks for the Aviation Accidents 
+ #      Project.
+ #
+ #
+ #  Date            Description                             Programmer
+ #  ----------      ------------------------------------    ------------------
+ #  10/05/2023      Initial Development                     N. James George
+ #
+ #******************************************************************************************/
+
+
+# In[2]:
+
+
+CONSTANT_LOCAL_FILE_NAME \
+    = 'PyAviationAccidentsSetupConstants.py'
+
+
+# In[3]:
+
+
+AVIATION_ACCIDENTS_URL \
+    = 'https://aviation-safety.net/database/year/'
+
+AVIATION_ACCIDENTS_DATA_FILE \
+    = './Resources/AviationSafetyData.csv'
+
+BEGIN_YEAR \
+    = 1972
+
+END_YEAR \
+    = 2022
+
