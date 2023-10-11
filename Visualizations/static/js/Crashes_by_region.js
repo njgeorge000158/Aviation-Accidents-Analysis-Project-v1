c99e7e6fa95c41b7ea789c0591cfabe494ac0b9c
tlbravo <tbravo507@gmail.com>
2023-10-11 16:14:03 -0400

    upload data folder to visualizations

--- Visualizations/static/js/Crashes_by_region.js
+++ Visualizations/static/js/Crashes_by_region.js
@@ -1,3 +1,5 @@
+
+
 let myMap = L.map("map", {
   center: [39.95226, -75.1652],
   zoom: 11
@@ -8,14 +10,8 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(myMap);
 
-let url = "https://raw.githubusercontent.com/njgeorge000158/Aviation-Crash-Visualization-Project/main/Resources/Aviation_Accident.json";
+let url = ""
 
 
-// d3.json(url).then(function(response) {
 
-//     console.log(response);
-//     features = response.features;
 
-//   }).addTo(myMap);
-
-// ;
