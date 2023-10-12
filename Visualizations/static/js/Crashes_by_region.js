ae94fd067ed2fc2340b18b08d5cd69f4a8c0d72d
tlbravo <tbravo507@gmail.com>
2023-10-12 13:22:28 -0400

    update to index.html and update to js file using flask api example

--- Visualizations/static/js/Crashes_by_region.js
+++ Visualizations/static/js/Crashes_by_region.js
@@ -1,41 +1,36 @@
+//Updates using localhost flask api example from rajib.
+let link = "http://localhost:8000/countrysearch?country=us";
 
+d3.json(link).then(function(data) {
+  let myData = data
 
-let myMap = L.map("map", {
-  center: [39.95226, -75.1652],
-  zoom: 11
-});
+  let myMap = L.map("map", {
+    center: [15.5994, -28.6731],
+    zoom: 3
+  });
 
 //Tile layer
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(myMap);
 
-let url = "https://raw.githubusercontent.com/njgeorge000158/Aviation-Crash-Visualization-Project/main/Visualizations/static/data/data1.js"
+// // Create a new marker cluster group.
+let markers = L.markerClusterGroup();
 
-// Get the data with d3.
-d3.json(url).then(function(response) {
+// let aviation_accidents = response[0];
 
-  // Create a new marker cluster group.
-  let markers = L.markerClusterGroup();
+for (let i = 0; i < myData.length; i++) {
 
-  let aviation_accidents = response[0];
+  let location = myData[i].location;
 
-  for (let i = 0; i < aviation_accidents.length; i++) {
+  if (location) {
 
-    let location = aviation_accidents[i].location;
-
-    if (location) {
-
-      let lat = aviation_accidents[i].LAT;
-      let lng = aviation_accidents[i].LNG;
-      let marker = L.marker([lat, lng]);
-      
-      markers.bindPopup(aviation_accidents[i].descriptor);
-
-      markers.addLayer(marker);
-    }
+    markers.addLayer(L.marker([myData[i].LAT,myData[i].LNG])
+    .bindPopup(`<h1>${myData[i].accident_date}</h1> <hr> <h3>Carrier Type: ${myData[i].type}</h3><hr> <h3>Operator: ${myData[i].operator}</h3>`));
   }
 
-  myMap.addLayer(markers);
+}
 
-});
+// Add our marker cluster layer to the map.
+myMap.addLayer(markers);}
+,);
