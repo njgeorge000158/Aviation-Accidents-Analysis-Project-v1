5c1d26d5f1e7bbdeda20c895597f9a3296c2aeae
majirajib4747 <43809492+majirajib4747@users.noreply.github.com>
2023-10-15 22:00:54 -0400

    Add files via upload

--- Aviation_Accident_Visualization/logic_plotly.js
+++ Aviation_Accident_Visualization/logic_plotly.js
@@ -1,35 +1,46 @@
 //console.log(aviation_accidents)
 // Use this link to get the GeoJSON data.
-let link = "http://127.0.0.1:8000/allyear";
+//let link = "http://127.0.0.1:8000/allyear";
+all_year_link = "http://127.0.0.1:8000/yearsearch?beg=1970&end=2030"
+display_graph(all_year_link)
 
-d3.json(link, function(data) {
+function display_graph(link)
+{
+  d3.json(link, function(data) {
   
-  let myData = data
-  //console.log(myData)
-  // Create a map object.
-  function init()
- {
-  let year = []
-  let accident_count = []
- for (let i = 0; i < myData.length; i++) {
-   year.push(myData[i].year)
-   accident_count.push(myData[i].count)
-   let title = `Accident Count Chart by Year`
-   let trace1 = {
-    x: year,
-    y: accident_count,
-    type: 'bar'
-  };
+    let myData = data
   
- let data = [trace1];
+    
+      let year = []
+      let accident_count = []
+      for (let i = 0; i < myData.length; i++) {
+          year.push(myData[i].year)
+          accident_count.push(myData[i].count)
+      }
+          
   
-  let layout = {
-    title: title
-  };
+          let title = `Accident Count Chart by Year`
+          let trace1 = {
+                x: year,
+                y: accident_count,
+                type: 'bar'
+          };
+          
+    
+          let data1 = [trace1];
+    
+          let layout = {
+              title: title
+          };
+    
+      Plotly.newPlot("plot", data1, layout);
+   
   
-  Plotly.newPlot("plot", data, layout);
- }
+
+});
 }
+
+
 d3.selectAll("#selYear").on("change", getData);
 
 function getData() {
@@ -37,179 +48,44 @@ function getData() {
   let dropdownMenu = d3.select("#selYear");
   // Assign the value of the dropdown menu option to a letiable
   let dataset = dropdownMenu.property("value");
-  // Initialize an empty array for the country's data
-  let year = []
-  let accident_count = []
-
+  
   if (dataset == '70s') {
-    for (let i = 0; i < myData.length; i++) {
-      if (Number(myData[i].year) > 1970 && Number(myData[i].year) < 1980 )
-      {
-       year.push(myData[i].year)
-       accident_count.push(myData[i].count)
-      }
-    }
-    let title = `Accident Count Chart by Year`
-      let trace1 = {
-       x: year,
-       y: accident_count,
-       type: 'bar'
-     };
-     let data = [trace1]; 
-     title = `Accident Count Chart by Year`
-      let layout = {
-    title: title
-  };
-
-  Plotly.newPlot("plot", data, layout);
+    year_link = "http://127.0.0.1:8000/yearsearch?beg=1970&end=1980"
+    display_graph(year_link)
   }
 
   else if (dataset == '80s') {
-    for (let i = 0; i < myData.length; i++) {
-      if (Number(myData[i].year) >= 1980 && Number(myData[i].year) < 1990 )
-      {
-       year.push(myData[i].year)
-       accident_count.push(myData[i].count)
-      }
-    }
-    let title = `Accident Count Chart by Year`
-      let trace1 = {
-       x: year,
-       y: accident_count,
-       type: 'bar'
-     };
-     let data = [trace1]; 
-     title = `Accident Count Chart by Year`
-      let layout = {
-    title: title
-  };
-
-  Plotly.newPlot("plot", data, layout);
+    year_link = "http://127.0.0.1:8000/yearsearch?beg=1980&end=1990"
+    display_graph(year_link)
   }
 
   else if (dataset == '90s') {
-    for (let i = 0; i < myData.length; i++) {
-      if (Number(myData[i].year) >= 1990 && Number(myData[i].year) < 2000 )
-      {
-       year.push(myData[i].year)
-       accident_count.push(myData[i].count)
-      }
-    }
-    let title = `Accident Count Chart by Year`
-      let trace1 = {
-       x: year,
-       y: accident_count,
-       type: 'bar'
-     };
-     let data = [trace1]; 
-     title = `Accident Count Chart by Year`
-      let layout = {
-    title: title
-  };
-
-  Plotly.newPlot("plot", data, layout);
+    year_link = "http://127.0.0.1:8000/yearsearch?beg=1990&end=2000"
+    display_graph(year_link)
   }
 
   else if (dataset == '2000') {
-    for (let i = 0; i < myData.length; i++) {
-      if (Number(myData[i].year) >= 2000 && Number(myData[i].year) < 2010 )
-      {
-       year.push(myData[i].year)
-       accident_count.push(myData[i].count)
-      }
-    }
-    let title = `Accident Count Chart by Year`
-      let trace1 = {
-       x: year,
-       y: accident_count,
-       type: 'bar'
-     };
-     let data = [trace1]; 
-     title = `Accident Count Chart by Year`
-      let layout = {
-    title: title
-  };
-
-  Plotly.newPlot("plot", data, layout);
+    year_link = "http://127.0.0.1:8000/yearsearch?beg=2000&end=2010"
+    display_graph(year_link)
   }
 
   else if (dataset == '2010') {
-    for (let i = 0; i < myData.length; i++) {
-      if (Number(myData[i].year) >= 2010 && Number(myData[i].year )< 2020 )
-      {
-       year.push(myData[i].year)
-       accident_count.push(myData[i].count)
-      }
-    }
-    let title = `Accident Count Chart by Year`
-      let trace1 = {
-       x: year,
-       y: accident_count,
-       type: 'bar'
-     };
-     let data = [trace1]; 
-     title = `Accident Count Chart by Year`
-      let layout = {
-    title: title
-  };
-
-  Plotly.newPlot("plot", data, layout);
+    year_link = "http://127.0.0.1:8000/yearsearch?beg=2010&end=2020"
+    display_graph(year_link)
   }
 
   else if (dataset == '2020')  {
-    for (let i = 0; i < myData.length; i++) {
-      if (Number(myData[i].year) >= 2020  )
-      {
-       year.push(myData[i].year)
-       accident_count.push(myData[i].count)
-      }
-    }
-    let title = `Accident Count Chart by Year`
-      let trace1 = {
-       x: year,
-       y: accident_count,
-       type: 'bar'
-     };
-     let data = [trace1]; 
-     title = `Accident Count Chart by Year`
-      let layout = {
-    title: title
-  };
-
-  Plotly.newPlot("plot", data, layout);
+    year_link = "http://127.0.0.1:8000/yearsearch?beg=2020&end=2030"
+    display_graph(year_link)
   }
 
   else   {
-    for (let i = 0; i < myData.length; i++) {
-      if (Number(myData[i].year) >= 1950 && Number(myData[i].year) <= 2030  )
-      {
-       year.push(myData[i].year)
-       accident_count.push(myData[i].count)
-      }
-    }
-    let title = `Accident Count Chart by Year`
-      let trace1 = {
-       x: year,
-       y: accident_count,
-       type: 'bar'
-     };
-     let data = [trace1]; 
-     title = `Accident Count Chart by Year`
-      let layout = {
-    title: title
-  };
-
-  Plotly.newPlot("plot", data, layout);
+    year_link = "http://127.0.0.1:8000/yearsearch?beg=1970&end=2030"
+    display_graph(year_link)
   }
-
-// Call function to update the chart
-
-//updatePlotly(data);
 
 }
 
-init();
-});
 
 
 
