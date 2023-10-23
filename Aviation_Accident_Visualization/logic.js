d9afec7eae067845da0dfe52e84cab621fc7ba20
majirajib4747 <43809492+majirajib4747@users.noreply.github.com>
2023-10-23 18:00:40 -0400

    Visulaization

---
+++ Aviation_Accident_Visualization/logic.js
@@ -0,0 +1,242 @@
+//console.log(aviation_accidents)
+// Use this link to get the GeoJSON data.
+let link = "http://127.0.0.1:8000/all";
+let myMap = L.map("map", {
+  center: [15.5994, -28.6731],
+  zoom: 3
+});
+d3.json("http://127.0.0.1:8000/alluniquecountry" , function (data) {
+      console.log(data);
+        // Create array to hold all names (all ID names)
+        var countries = data.map(x=>x.country)
+        // Append an option in the dropdown
+        countries.forEach(function(country) {
+            d3.select('#selCountry')
+                .append('option')
+                .text(country)
+            });
+          });          
+
+initial_graph(link)
+
+$(document).ready(function(){
+  let newlink = ""
+  // Get value on button click and show alert
+  $("#YearBtn").click(function(){
+      var str = $("#yearInput").val();
+      //console.log("str" + len(str));
+      if (str.length != 4) 
+       {
+        newlink = "http://127.0.0.1:8000/all" 
+        initial_graph(newlink)
+       }
+      else
+      {
+        newlink = "http://127.0.0.1:8000/year?year=" + str;
+        display_graph(newlink)
+      } 
+      
+      
+  });
+  $("#CarrierBtn").click(function(){
+    var carrierstr = $("#carrierInput").val();
+    //console.log("str" + len(str));
+    if (carrierstr.length == 0) 
+     {
+      newlink = "http://127.0.0.1:8000/all" 
+      initial_graph(newlink)
+     }
+    else
+    {
+      newlink = "http://127.0.0.1:8000//carriertypesearch?carriertype=" + carrierstr;
+      display_graph(newlink)
+    } 
+    
+    
+});
+});
+
+function countryChange (countryname)
+{
+  let countrylink = "http://127.0.0.1:8000/countrysearch?country=" + countryname ; 
+  myMap.off();
+  myMap.remove();  
+  myMap = L.map("map", {
+    center: [15.5994, -28.6731],
+    zoom: 3
+  });
+// Getting our GeoJSON data
+d3.json(countrylink, function(data) {
+  // Creating a GeoJSON layer with the retrieved data
+  let myData = data
+  //console.log(myData)
+  // Create a map object.
+  
+  //let myMap = L.map("map", {
+  //center: [15.5994, -28.6731],
+ // zoom: 3
+//});
+
+// Add a tile layer.
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(myMap);
+
+
+// Loop through the countries array, and create one marker for each country object.
+//for (let i = 0; i < aviation_accidents.length; i++) {
+for (let i = 0; i < myData.length; i++) {
+  // Conditionals for country gdp_pc
+  let color = "";
+  //if (aviation_accidents[i].fat > 200) {
+  if (myData[i].fat > 200) {
+    color = "yellow";
+  }
+  //else if (aviation_accidents[i].fat > 150) {
+  else if (myData[i].fat > 150) {  
+    color = "blue";
+  }
+  //else if (aviation_accidents[i].fat > 120) {
+  else if (myData[i].fat > 120) {
+    color = "green";
+  }
+  else {
+    color = "violet";
+  }
+
+  // Add circles to the map.
+  //L.circle(([aviation_accidents[i].LAT,aviation_accidents[i].LNG]), {
+  L.circle(([myData[i].LAT,myData[i].LNG]), {
+    fillOpacity: 0.75,
+    color: "white",
+    fillColor: color,
+    // Adjust the radius.
+    //radius: Math.sqrt(aviation_accidents[i].fat) * 20000
+    radius: Math.sqrt(myData[i].fat) * 20000
+  //}).bindPopup(`<h1>${aviation_accidents[i].accident_date}</h1> <hr> <h3>Carrier Type: ${aviation_accidents[i].type}</h3><hr> <h3>Operator: ${aviation_accidents[i].operator}</h3>`).addTo(myMap);
+  }).bindPopup(`<h1>${myData[i].accident_date}</h1> <hr> <h3>Carrier Type: ${myData[i].carrier_type}</h3><hr> <h3>Operator: ${myData[i].operator}</h3>`).addTo(myMap);
+}
+});
+
+}
+function display_graph(link)
+{
+  myMap.off();
+  myMap.remove();  
+  myMap = L.map("map", {
+    center: [15.5994, -28.6731],
+    zoom: 3
+  });
+// Getting our GeoJSON data
+d3.json(link, function(data) {
+  // Creating a GeoJSON layer with the retrieved data
+  let myData = data
+  //console.log(myData)
+  // Create a map object.
+  
+  //let myMap = L.map("map", {
+  //center: [15.5994, -28.6731],
+ // zoom: 3
+//});
+
+// Add a tile layer.
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(myMap);
+
+
+// Loop through the countries array, and create one marker for each country object.
+//for (let i = 0; i < aviation_accidents.length; i++) {
+for (let i = 0; i < myData.length; i++) {
+  // Conditionals for country gdp_pc
+  let color = "";
+  //if (aviation_accidents[i].fat > 200) {
+  if (myData[i].fat > 200) {
+    color = "yellow";
+  }
+  //else if (aviation_accidents[i].fat > 150) {
+  else if (myData[i].fat > 150) {  
+    color = "blue";
+  }
+  //else if (aviation_accidents[i].fat > 120) {
+  else if (myData[i].fat > 120) {
+    color = "green";
+  }
+  else {
+    color = "violet";
+  }
+
+  // Add circles to the map.
+  //L.circle(([aviation_accidents[i].LAT,aviation_accidents[i].LNG]), {
+  L.circle(([myData[i].LAT,myData[i].LNG]), {
+    fillOpacity: 0.75,
+    color: "white",
+    fillColor: color,
+    // Adjust the radius.
+    //radius: Math.sqrt(aviation_accidents[i].fat) * 20000
+    radius: Math.sqrt(myData[i].fat) * 20000
+  //}).bindPopup(`<h1>${aviation_accidents[i].accident_date}</h1> <hr> <h3>Carrier Type: ${aviation_accidents[i].type}</h3><hr> <h3>Operator: ${aviation_accidents[i].operator}</h3>`).addTo(myMap);
+  }).bindPopup(`<h1>${myData[i].accident_date}</h1> <hr> <h3>Carrier Type: ${myData[i].carrier_type}</h3><hr> <h3>Operator: ${myData[i].operator}</h3>`).addTo(myMap);
+}
+});
+
+}
+
+function initial_graph(link)
+{
+// Getting our GeoJSON data
+d3.json(link, function(data) {
+  // Creating a GeoJSON layer with the retrieved data
+  let myData = data
+  //console.log(myData)
+  // Create a map object.
+  
+  //let myMap = L.map("map", {
+ // center: [15.5994, -28.6731],
+ // zoom: 3
+//});
+
+// Add a tile layer.
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(myMap);
+
+
+// Loop through the countries array, and create one marker for each country object.
+//for (let i = 0; i < aviation_accidents.length; i++) {
+for (let i = 0; i < myData.length; i++) {
+  // Conditionals for country gdp_pc
+  let color = "";
+  //if (aviation_accidents[i].fat > 200) {
+  if (myData[i].fat > 200) {
+    color = "yellow";
+  }
+  //else if (aviation_accidents[i].fat > 150) {
+  else if (myData[i].fat > 150) {  
+    color = "blue";
+  }
+  //else if (aviation_accidents[i].fat > 120) {
+  else if (myData[i].fat > 120) {
+    color = "green";
+  }
+  else {
+    color = "violet";
+  }
+
+  // Add circles to the map.
+  //L.circle(([aviation_accidents[i].LAT,aviation_accidents[i].LNG]), {
+  L.circle(([myData[i].LAT,myData[i].LNG]), {
+    fillOpacity: 0.75,
+    color: "white",
+    fillColor: color,
+    // Adjust the radius.
+    //radius: Math.sqrt(aviation_accidents[i].fat) * 20000
+    radius: Math.sqrt(myData[i].fat) * 20000
+  //}).bindPopup(`<h1>${aviation_accidents[i].accident_date}</h1> <hr> <h3>Carrier Type: ${aviation_accidents[i].type}</h3><hr> <h3>Operator: ${aviation_accidents[i].operator}</h3>`).addTo(myMap);
+  }).bindPopup(`<h1>${myData[i].accident_date}</h1> <hr> <h3>Carrier Type: ${myData[i].type}</h3><hr> <h3>Operator: ${myData[i].operator}</h3>`).addTo(myMap);
+}
+});
+
+}
+
+
---
+++ Aviation_Accident_Visualization/logic_plotly.js
@@ -0,0 +1,92 @@
+//console.log(aviation_accidents)
+// Use this link to get the GeoJSON data.
+//let link = "http://127.0.0.1:8000/allyear";
+all_year_link = "http://127.0.0.1:8000/yearsearch?beg=1970&end=2030"
+display_graph(all_year_link)
+
+function display_graph(link)
+{
+  d3.json(link, function(data) {
+  
+    let myData = data
+  
+    
+      let year = []
+      let accident_count = []
+      for (let i = 0; i < myData.length; i++) {
+          year.push(myData[i].year)
+          accident_count.push(myData[i].count)
+      }
+          
+  
+          let title = `Accident Count Chart by Year`
+          let trace1 = {
+                x: year,
+                y: accident_count,
+                type: 'bar'
+          };
+          
+    
+          let data1 = [trace1];
+    
+          let layout = {
+              title: title
+          };
+    
+      Plotly.newPlot("plot", data1, layout);
+   
+  
+
+});
+}
+
+
+d3.selectAll("#selYear").on("change", getData);
+
+function getData() {
+
+  let dropdownMenu = d3.select("#selYear");
+  // Assign the value of the dropdown menu option to a letiable
+  let dataset = dropdownMenu.property("value");
+  
+  if (dataset == '70s') {
+    year_link = "http://127.0.0.1:8000/yearsearch?beg=1970&end=1980"
+    display_graph(year_link)
+  }
+
+  else if (dataset == '80s') {
+    year_link = "http://127.0.0.1:8000/yearsearch?beg=1980&end=1990"
+    display_graph(year_link)
+  }
+
+  else if (dataset == '90s') {
+    year_link = "http://127.0.0.1:8000/yearsearch?beg=1990&end=2000"
+    display_graph(year_link)
+  }
+
+  else if (dataset == '2000') {
+    year_link = "http://127.0.0.1:8000/yearsearch?beg=2000&end=2010"
+    display_graph(year_link)
+  }
+
+  else if (dataset == '2010') {
+    year_link = "http://127.0.0.1:8000/yearsearch?beg=2010&end=2020"
+    display_graph(year_link)
+  }
+
+  else if (dataset == '2020')  {
+    year_link = "http://127.0.0.1:8000/yearsearch?beg=2020&end=2030"
+    display_graph(year_link)
+  }
+
+  else   {
+    year_link = "http://127.0.0.1:8000/yearsearch?beg=1970&end=2030"
+    display_graph(year_link)
+  }
+
+}
+
+
+
+
+
