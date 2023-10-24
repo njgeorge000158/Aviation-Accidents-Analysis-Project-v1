93c8b6f5380b0f7fd45c6abf97d1e46b845e5bd6
Theresa Bravo <133545219+tlbravo@users.noreply.github.com>
2023-10-24 12:09:57 -0400

    Add files via upload

--- Aviation_Accident_Visualization/logic.js
+++ Aviation_Accident_Visualization/logic.js
@@ -1,6 +1,27 @@
 //console.log(aviation_accidents)
 // Use this link to get the GeoJSON data.
 let link = "http://127.0.0.1:8000/all";
+initial_graph(link)
+  function colorchooser(fat)
+  {
+  let colorchooser = "";
+  //if (aviation_accidents[i].fat > 200) {
+  if (fat > 200) {
+    colorchooser = "#b80410";
+  }
+  //else if (aviation_accidents[i].fat > 150) {
+  else if (fat > 150) {
+    colorchooser = "#ed8a3e";
+  }
+  //else if (aviation_accidents[i].fat > 120) {
+  else if (fat > 120) {
+    colorchooser = "#c3eb13";
+  }
+  else {
+    colorchooser = "#4a04c2";
+  }
+  return colorchooser;
+  }
 let myMap = L.map("map", {
   center: [15.5994, -28.6731],
   zoom: 3
@@ -15,52 +36,46 @@ d3.json("http://127.0.0.1:8000/alluniquecountry" , function (data) {
                 .append('option')
                 .text(country)
             });
-          });          
-
-initial_graph(link)
-
+          });
+//initial_graph(link)
 $(document).ready(function(){
   let newlink = ""
   // Get value on button click and show alert
   $("#YearBtn").click(function(){
       var str = $("#yearInput").val();
       //console.log("str" + len(str));
-      if (str.length != 4) 
+      if (str.length != 4)
        {
-        newlink = "http://127.0.0.1:8000/all" 
+        newlink = "http://127.0.0.1:8000/all"
         initial_graph(newlink)
        }
       else
       {
         newlink = "http://127.0.0.1:8000/year?year=" + str;
         display_graph(newlink)
-      } 
-      
-      
+      }
   });
   $("#CarrierBtn").click(function(){
     var carrierstr = $("#carrierInput").val();
     //console.log("str" + len(str));
-    if (carrierstr.length == 0) 
+    if (carrierstr.length == 0)
      {
-      newlink = "http://127.0.0.1:8000/all" 
+      newlink = "http://127.0.0.1:8000/all"
       initial_graph(newlink)
      }
     else
     {
       newlink = "http://127.0.0.1:8000//carriertypesearch?carriertype=" + carrierstr;
       display_graph(newlink)
-    } 
-    
-    
-});
-});
+    }
 
+});
+});
 function countryChange (countryname)
 {
-  let countrylink = "http://127.0.0.1:8000/countrysearch?country=" + countryname ; 
+  let countrylink = "http://127.0.0.1:8000/countrysearch?country=" + countryname ;
   myMap.off();
-  myMap.remove();  
+  myMap.remove();
   myMap = L.map("map", {
     center: [15.5994, -28.6731],
     zoom: 3
@@ -71,18 +86,14 @@ d3.json(countrylink, function(data) {
   let myData = data
   //console.log(myData)
   // Create a map object.
-  
   //let myMap = L.map("map", {
   //center: [15.5994, -28.6731],
  // zoom: 3
 //});
-
 // Add a tile layer.
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(myMap);
-
-
 // Loop through the countries array, and create one marker for each country object.
 //for (let i = 0; i < aviation_accidents.length; i++) {
 for (let i = 0; i < myData.length; i++) {
@@ -90,20 +101,19 @@ for (let i = 0; i < myData.length; i++) {
   let color = "";
   //if (aviation_accidents[i].fat > 200) {
   if (myData[i].fat > 200) {
-    color = "yellow";
+    color = "#b80410";
   }
   //else if (aviation_accidents[i].fat > 150) {
-  else if (myData[i].fat > 150) {  
-    color = "blue";
+  else if (myData[i].fat > 150) {
+    color = "#ed8a3e";
   }
   //else if (aviation_accidents[i].fat > 120) {
   else if (myData[i].fat > 120) {
-    color = "green";
+    color = "#c3eb13";
   }
   else {
-    color = "violet";
+    color = "#4a04c2";
   }
-
   // Add circles to the map.
   //L.circle(([aviation_accidents[i].LAT,aviation_accidents[i].LNG]), {
   L.circle(([myData[i].LAT,myData[i].LNG]), {
@@ -115,14 +125,31 @@ for (let i = 0; i < myData.length; i++) {
     radius: Math.sqrt(myData[i].fat) * 20000
   //}).bindPopup(`<h1>${aviation_accidents[i].accident_date}</h1> <hr> <h3>Carrier Type: ${aviation_accidents[i].type}</h3><hr> <h3>Operator: ${aviation_accidents[i].operator}</h3>`).addTo(myMap);
   }).bindPopup(`<h1>${myData[i].accident_date}</h1> <hr> <h3>Carrier Type: ${myData[i].carrier_type}</h3><hr> <h3>Operator: ${myData[i].operator}</h3>`).addTo(myMap);
+  
+  if (myMap.legend) {
+    myMap.legend.remove(myMap);
+  }
+  let legend = L.control({position: "bottomleft"});
+  legend.onAdd = function () {
+    let div = L.DomUtil.create("div", "info legend");
+    fatalityScale = [0, 120, 150, 200];
+    div.innerHTML += "<h3 style = 'text-align: center'>Fatalities</h3>";
+    for (let i=0; i < fatalityScale.length; i++){
+      div.innerHTML +=
+      '<i style="background:' + colorchooser(fatalityScale[i] +1) + '"></i>' +
+      fatalityScale [i] + (fatalityScale[i+1] ? '&ndash;' + fatalityScale[i+1]+'<br>': '+');
+    }
+    return div;
+  };
+  legend.addTo(myMap);
+  myMap.legend = legend;
 }
 });
-
 }
 function display_graph(link)
 {
   myMap.off();
-  myMap.remove();  
+  myMap.remove();
   myMap = L.map("map", {
     center: [15.5994, -28.6731],
     zoom: 3
@@ -133,18 +160,14 @@ d3.json(link, function(data) {
   let myData = data
   //console.log(myData)
   // Create a map object.
-  
   //let myMap = L.map("map", {
   //center: [15.5994, -28.6731],
  // zoom: 3
 //});
-
 // Add a tile layer.
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(myMap);
-
-
 // Loop through the countries array, and create one marker for each country object.
 //for (let i = 0; i < aviation_accidents.length; i++) {
 for (let i = 0; i < myData.length; i++) {
@@ -152,20 +175,19 @@ for (let i = 0; i < myData.length; i++) {
   let color = "";
   //if (aviation_accidents[i].fat > 200) {
   if (myData[i].fat > 200) {
-    color = "yellow";
+    color = "#b80410";
   }
   //else if (aviation_accidents[i].fat > 150) {
-  else if (myData[i].fat > 150) {  
-    color = "blue";
+  else if (myData[i].fat > 150) {
+    color = "#ed8a3e";
   }
   //else if (aviation_accidents[i].fat > 120) {
   else if (myData[i].fat > 120) {
-    color = "green";
+    color = "#c3eb13";
   }
   else {
-    color = "violet";
+    color = "#4a04c2";
   }
-
   // Add circles to the map.
   //L.circle(([aviation_accidents[i].LAT,aviation_accidents[i].LNG]), {
   L.circle(([myData[i].LAT,myData[i].LNG]), {
@@ -177,11 +199,28 @@ for (let i = 0; i < myData.length; i++) {
     radius: Math.sqrt(myData[i].fat) * 20000
   //}).bindPopup(`<h1>${aviation_accidents[i].accident_date}</h1> <hr> <h3>Carrier Type: ${aviation_accidents[i].type}</h3><hr> <h3>Operator: ${aviation_accidents[i].operator}</h3>`).addTo(myMap);
   }).bindPopup(`<h1>${myData[i].accident_date}</h1> <hr> <h3>Carrier Type: ${myData[i].carrier_type}</h3><hr> <h3>Operator: ${myData[i].operator}</h3>`).addTo(myMap);
+  
+  if (myMap.legend) {
+    myMap.legend.remove(myMap);
+  } 
+  
+  let legend = L.control({position: "bottomleft"});
+  legend.onAdd = function () {
+    let div = L.DomUtil.create("div", "info legend");
+    fatalityScale = [0, 120, 150, 200];
+    div.innerHTML += "<h3 style = 'text-align: center'>Fatalities</h3>";
+    for (let i=0; i < fatalityScale.length; i++){
+      div.innerHTML +=
+      '<i style="background:' + colorChooser(fatalityScale[i] +1) + '"></i>' +
+      fatalityScale [i] + (fatalityScale[i+1] ? '&ndash;' + fatalityScale[i+1]+'<br>': '+');
+    }
+    return div;
+  };
+  legend.addTo(myMap);
+  myMap.legend = legend;
 }
 });
-
 }
-
 function initial_graph(link)
 {
 // Getting our GeoJSON data
@@ -190,18 +229,14 @@ d3.json(link, function(data) {
   let myData = data
   //console.log(myData)
   // Create a map object.
-  
   //let myMap = L.map("map", {
  // center: [15.5994, -28.6731],
  // zoom: 3
 //});
-
 // Add a tile layer.
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(myMap);
-
-
 // Loop through the countries array, and create one marker for each country object.
 //for (let i = 0; i < aviation_accidents.length; i++) {
 for (let i = 0; i < myData.length; i++) {
@@ -209,20 +244,19 @@ for (let i = 0; i < myData.length; i++) {
   let color = "";
   //if (aviation_accidents[i].fat > 200) {
   if (myData[i].fat > 200) {
-    color = "yellow";
+    color = "#b80410";
   }
   //else if (aviation_accidents[i].fat > 150) {
-  else if (myData[i].fat > 150) {  
-    color = "blue";
+  else if (myData[i].fat > 150) {
+    color = "#ed8a3e";
   }
   //else if (aviation_accidents[i].fat > 120) {
   else if (myData[i].fat > 120) {
-    color = "green";
+    color = "#c3eb13";
   }
   else {
-    color = "violet";
+    color = "#4a04c2";
   }
-
   // Add circles to the map.
   //L.circle(([aviation_accidents[i].LAT,aviation_accidents[i].LNG]), {
   L.circle(([myData[i].LAT,myData[i].LNG]), {
@@ -234,9 +268,27 @@ for (let i = 0; i < myData.length; i++) {
     radius: Math.sqrt(myData[i].fat) * 20000
   //}).bindPopup(`<h1>${aviation_accidents[i].accident_date}</h1> <hr> <h3>Carrier Type: ${aviation_accidents[i].type}</h3><hr> <h3>Operator: ${aviation_accidents[i].operator}</h3>`).addTo(myMap);
   }).bindPopup(`<h1>${myData[i].accident_date}</h1> <hr> <h3>Carrier Type: ${myData[i].type}</h3><hr> <h3>Operator: ${myData[i].operator}</h3>`).addTo(myMap);
+  
+  if (myMap.legend) {
+    myMap.legend.remove(myMap);
+  }
+  
+  let legend = L.control({position: "bottomleft"});
+  legend.onAdd = function () {
+    let div = L.DomUtil.create("div", "info legend");
+    fatalityScale = [0, 120, 150, 200];
+    div.innerHTML += "<h3 style = 'text-align: center'>Fatalities</h3>";
+    for (let i=0; i < fatalityScale.length; i++){
+      div.innerHTML +=
+      '<i style="background:' + colorchooser(fatalityScale[i] +1) + '"></i>' +
+      fatalityScale [i] + (fatalityScale[i+1] ? '&ndash;' + fatalityScale[i+1]+'<br>': '+');
+    }
+    return div;
+  };
+  legend.addTo(myMap);
+  myMap.legend = legend;
 }
 });
-
-}
+};
 
 
